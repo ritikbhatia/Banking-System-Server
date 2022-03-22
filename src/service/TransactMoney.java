0d1204bbb545bc8d36b306028f6fb03ff7de8b84
Aditya Bansal <adityabansal10@gmail.com>
2022-03-22 16:45:23 +0800

    Adds Transact Money Service

---
+++ src/service/TransactMoney.java
@@ -0,0 +1,58 @@
+package service;
+
+import entity.Account;
+import entity.Currency;
+import utils.Response;
+import utils.Response.Status;
+
+public class TransactMoney extends Service{
+
+    public Response depositMoney(int accountNumber, String accountHolderName, String password, Currency currency, double amount){
+        
+        Response response = checkAccountDetails(accountNumber, accountHolderName, password);
+       
+        if(response.getStatus() == Status.FAILURE){
+           return response;
+        }
+
+        Account bankAccount = bank.getAccount(accountNumber);
+        
+        if(bankAccount.getCurrency() != currency){
+            String mssg = "Currency does not match.";
+            return new Response(Status.FAILURE, mssg);
+        }
+        
+        else{
+            bankAccount.setBalance(bankAccount.getBalance() + amount);
+            String mssg = currency.toString() + " " + String.valueOf(amount) + " deposited into account successfully.";
+            return new Response(Status.SUCCESS, mssg);
+        }
+    }
+    
+    public Response withdrawMoney(int accountNumber, String accountHolderName, String password, Currency currency, double amount){
+        
+        Response response = checkAccountDetails(accountNumber, accountHolderName, password);
+       
+        if(response.getStatus() == Status.FAILURE){
+           return response;
+        }
+
+        Account bankAccount = bank.getAccount(accountNumber);
+        
+        if(bankAccount.getCurrency() != currency){
+            String mssg = "Currency does not match.";
+            return new Response(Status.FAILURE, mssg);
+        }
+        
+        else if(bankAccount.getBalance() < amount){
+            String mssg = "Insufficient balance.";
+            return new Response(Status.FAILURE, mssg);
+        }
+        
+        else{
+            bankAccount.setBalance(bankAccount.getBalance() - amount);
+            String mssg = currency.toString() + " " + String.valueOf(amount) + " withdrawn from account successfully.";
+            return new Response(Status.SUCCESS, mssg);
+        }
+    }
+}
