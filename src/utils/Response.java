7f1d5c35902b32ea4aa7111ba0797c2b147b585d
Aditya Bansal <adityabansal10@gmail.com>
2022-03-22 16:06:01 +0800

    Adds Response class

---
+++ src/utils/Response.java
@@ -0,0 +1,26 @@
+package utils;
+
+public class Response {
+
+    public enum Status{
+        SUCCESS,
+        FAILURE 
+        
+    }
+
+    private Status status; 
+    private String message;
+    
+    public Response(Status status, String message){
+        this.status = status;
+        this.message = message;
+    }
+    
+    public Status getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
