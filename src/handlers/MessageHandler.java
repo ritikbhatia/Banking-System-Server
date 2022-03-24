76a115f688c5569fcaf04bee76612b734d5e8ce8
ritikbhatia <bhatia.ritik5@gmail.com>
2022-03-24 23:28:23 +0800

    Unified communication handler

--- src/handlers/MessageHandler.java
+++ src/handlers/MessageHandler.java
@@ -3,16 +3,54 @@ package handlers;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.nio.charset.StandardCharsets;
+import system.message.Response.Status;
 
 import system.message.*;
 
-public class ClientMessageHandler {
+public class MessageHandler {
 
     public static final byte UPCOMING_STRING = 0x01;
     public static final byte UPCOMING_INT = 0x02;
     public static final byte UPCOMING_FLOAT = 0x03;
 
-    public static Request unmarshalRequest(byte[] packetData) {
+    // making class member as Enum.values() is expensive
+    private static Status[] statusValues = Status.values();
+
+    public static Response unmarshalServerResponse(byte[] packetData) {
+        int index = 0;
+        ByteBuffer extractor = ByteBuffer.wrap(packetData);
+
+        // retrive type of status (FAILURE / SUCCESS)
+        int statusType = extractor.getInt(index);
+        index += Integer.BYTES;
+
+        // retrieve length of the message string
+        int messageLen = extractor.getInt(index);
+        index += Integer.BYTES;
+
+        // retrieve the message string
+        String message = new String(Arrays.copyOfRange(packetData, index, index + messageLen), StandardCharsets.UTF_16);
+
+        return new Response(statusValues[statusType], message);
+    }
+
+    public static byte[] marshalServerResponse(Response resp) {
+        int statusType = resp.getStatus().ordinal();
+        String messageString = resp.getMessage();
+        int messageLen = messageString.length();
+
+        int contentLen = Integer.BYTES + messageLen;
+        ByteBuffer marshalledMesage = ByteBuffer.allocate(contentLen);
+
+        marshalledMesage
+                .putInt(statusType)
+                .putInt(messageLen)
+                .put(messageString.getBytes());
+
+        return marshalledMesage.array();
+    }
+
+    public static Request unmarshalClientRequest(byte[] packetData) {
         int index = 0;
         ByteBuffer extractor = ByteBuffer.wrap(packetData);
 
@@ -49,18 +87,10 @@ public class ClientMessageHandler {
 
         return new Request(requestId, opType, arguments);
     }
+
     // TODO: implement marshalling
-
-    public static byte[] marshalRequest(Request resp) {
+    public static byte[] marshalClientRequest(Request resp) {
         return new byte[1024];
     }
 
-    public static byte[] marshalResponse(Response resp) {
-        return new byte[1024];
-    }
-
-    
-    public static Response unmarshalResponse(byte[] packetData) {
-        return new Response(Response.Status.SUCCESS," ");
-    }
 }
