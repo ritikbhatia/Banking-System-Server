f76ed143de2a45585c0368299ff2aa4e2a6d7c6f
ritikbhatia <bhatia.ritik5@gmail.com>
2022-03-25 00:08:27 +0800

    Marshalling and unmarshalling

--- src/client/ClientInterface.java
+++ src/client/ClientInterface.java
@@ -7,6 +7,7 @@ import bank.entities.Account;
 import bank.entities.OpType;
 import handlers.MessageHandler;
 import system.message.*;
+
 public class ClientInterface {
 
     DatagramSocket ds;
@@ -14,85 +15,73 @@ public class ClientInterface {
     int server_port;
     static int request_id;
 
-    ClientInterface(int port)
-    {
-        try{
+    ClientInterface(int port) {
+        try {
             ds = new DatagramSocket();
             server_port = port;
             request_id = 0;
-        }
-        catch(Exception e)
-        {
-            System.out.println("SOCKET EXCEPTION RAISED!!"+e);
+        } catch (Exception e) {
+            System.out.println("SOCKET EXCEPTION RAISED!!" + e);
         }
 
     }
 
-    void set_server_ip(InetAddress ip)
-    {
+    void set_server_ip(InetAddress ip) {
         server_ip = ip;
     }
 
-
-    Response openAccount(Account account)
-    {
-        Request request = new Request(request_id, OpType.CREATE_ACCOUNT.getCode(),new Object[]{account});
-        byte[] content = MessageHandler.marshalRequest(request);
+    Response openAccount(Account account) {
+        Request request = new Request(request_id, OpType.CREATE_ACCOUNT.getCode(), new Object[] { account });
+        byte[] content = MessageHandler.marshalClientRequest(request);
         sendRequest(server_ip, server_port, content);
-        Response response = receiveResponse();//return reply.getContent();
+        Response response = receiveResponse();// return reply.getContent();
         return response;
     }
 
-
-    Response closeAccount(Account account)
-    {
-        Request request = new Request(request_id, OpType.CLOSE_ACCOUNT.getCode(),new Object[]{account});
-        byte[] content = MessageHandler.marshalRequest(request);
+    Response closeAccount(Account account) {
+        Request request = new Request(request_id, OpType.CLOSE_ACCOUNT.getCode(), new Object[] { account });
+        byte[] content = MessageHandler.marshalClientRequest(request);
         sendRequest(server_ip, server_port, content);
         Response response = receiveResponse();
         return response;
     }
 
-    Response depositMoney(Account account, int amount)
-    {
-        Request request = new Request(request_id, OpType.DEPOSIT_MONEY.getCode(),new Object[]{account,amount});
-        byte[] content = MessageHandler.marshalRequest(request);
+    Response depositMoney(Account account, int amount) {
+        Request request = new Request(request_id, OpType.DEPOSIT_MONEY.getCode(), new Object[] { account, amount });
+        byte[] content = MessageHandler.marshalClientRequest(request);
         sendRequest(server_ip, server_port, content);
         Response response = receiveResponse();
         return response;
     }
 
-    Response withdrawMoney(Account account, int amount)
-    {
-        Request request = new Request(request_id, OpType.WITHDRAW_MONEY.getCode(),new Object[]{account,amount});
-        byte[] content = MessageHandler.marshalRequest(request);
+    Response withdrawMoney(Account account, int amount) {
+        Request request = new Request(request_id, OpType.WITHDRAW_MONEY.getCode(), new Object[] { account, amount });
+        byte[] content = MessageHandler.marshalClientRequest(request);
         sendRequest(server_ip, server_port, content);
         Response response = receiveResponse();
         return response;
     }
 
-    Response transferMoney(Account accountFrom, Account accountTo, int amount)
-    {
-        Request request = new Request(request_id, OpType.TRANSFER_MONEY.getCode(),new Object[]{accountFrom, accountTo, amount});
-        byte[] content = MessageHandler.marshalRequest(request);
+    Response transferMoney(Account accountFrom, Account accountTo, int amount) {
+        Request request = new Request(request_id, OpType.TRANSFER_MONEY.getCode(),
+                new Object[] { accountFrom, accountTo, amount });
+        byte[] content = MessageHandler.marshalClientRequest(request);
         sendRequest(server_ip, server_port, content);
         Response response = receiveResponse();
         return response;
     }
 
-    Response transactionHistory(Account account)
-    {
-        Request request = new Request(request_id, OpType.TRANSACTION_HISTORY.getCode(),new Object[]{account});
-        byte[] content = MessageHandler.marshalRequest(request);
+    Response transactionHistory(Account account) {
+        Request request = new Request(request_id, OpType.TRANSACTION_HISTORY.getCode(), new Object[] { account });
+        byte[] content = MessageHandler.marshalClientRequest(request);
         sendRequest(server_ip, server_port, content);
         Response response = receiveResponse();
         return response;
     }
 
-    Response monitorUpdates(Account account)
-    {
-        Request request = new Request(request_id, OpType.MONITOR_UPDATES.getCode(),new Object[]{account});
-        byte[] content = MessageHandler.marshalRequest(request);
+    Response monitorUpdates(Account account) {
+        Request request = new Request(request_id, OpType.MONITOR_UPDATES.getCode(), new Object[] { account });
+        byte[] content = MessageHandler.marshalClientRequest(request);
         sendRequest(server_ip, server_port, content);
         Response response = receiveResponse();
         return response;
@@ -100,7 +89,7 @@ public class ClientInterface {
 
     void sendRequest(InetAddress address, int port, byte[] requestBytes) {
         int retry = 0;
-        while(retry<3){
+        while (retry < 3) {
             try {
                 DatagramPacket packet = new DatagramPacket(requestBytes, requestBytes.length, address, port);
                 ds.send(packet);
@@ -113,38 +102,36 @@ public class ClientInterface {
         }
     }
 
-    Response receiveResponse()
-    {
+    Response receiveResponse() {
         byte[] responseBytes = new byte[1024];
         DatagramPacket packet = new DatagramPacket(responseBytes, responseBytes.length);
-        while(true){
-            try{
+        while (true) {
+            try {
                 ds.receive(packet);
                 break;
-            } catch(SocketTimeoutException e){
+            } catch (SocketTimeoutException e) {
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         byte[] msg = packet.getData();
-        Response response = MessageHandler.unmarshalResponse(msg);
+        Response response = MessageHandler.unmarshalServerResponse(msg);
         return response;
     }
 
+    // DatagramSocket ds = new DatagramSocket();
+    // InetAddress ip = InetAddress.getLocalHost();
+    // String message = "Random Crap";
+    // byte buf[] = null;
+    // buf = message.getBytes();
+    // DatagramPacket DpSend = new DatagramPacket(buf, buf.length, ip, 1234);
+    // ds.send(DpSend);
 
-        // DatagramSocket ds = new DatagramSocket();
-        // InetAddress ip = InetAddress.getLocalHost();
-        // String message = "Random Crap";
-        // byte buf[] = null;
-        // buf = message.getBytes();
-        // DatagramPacket DpSend = new DatagramPacket(buf, buf.length, ip, 1234);
-        // ds.send(DpSend);
-
-        // // loop while user not enters "bye"
-        // while (true) {
-        //     String inp = in.readLine();
-        //     if (message.equals("bye"))
-        //         break;
-        // }
-        // ds.close();
+    // // loop while user not enters "bye"
+    // while (true) {
+    // String inp = in.readLine();
+    // if (message.equals("bye"))
+    // break;
+    // }
+    // ds.close();
 }
--- src/handlers/MessageHandler.java
+++ src/handlers/MessageHandler.java
@@ -1,6 +1,7 @@
 package handlers;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.nio.charset.StandardCharsets;
 import system.message.Response.Status;
@@ -16,38 +17,48 @@ public class MessageHandler {
     // making class member as Enum.values() is expensive
     private static Status[] statusValues = Status.values();
 
-    public static Response unmarshalServerResponse(byte[] packetData) {
-        int index = 0;
-        ByteBuffer extractor = ByteBuffer.wrap(packetData);
+    public static byte[] marshalClientRequest(Request req) {
+        int requestId = req.getId();
+        int opType = req.getType();
 
-        // retrive type of status (FAILURE / SUCCESS)
-        int statusType = extractor.getInt(index);
-        index += Integer.BYTES;
+        // the first entry to specify the number of arguments
+        int argumentContentLength = Integer.BYTES;
+        ArrayList<byte[]> argumentContent = new ArrayList<>();
 
-        // retrieve length of the message string
-        int messageLen = extractor.getInt(index);
-        index += Integer.BYTES;
+        for (Object obj : req.getArguments()) {
+            if (obj instanceof String) {
+                byte[] strArg = ((String) obj).getBytes(StandardCharsets.UTF_16);
+                argumentContent.add(ByteBuffer.allocate(Byte.BYTES + Integer.BYTES + strArg.length)
+                        .put(UPCOMING_STRING)
+                        .putInt(strArg.length)
+                        .put(strArg)
+                        .array());
+                argumentContentLength += Byte.BYTES + Float.BYTES + strArg.length;
+            } else if (obj instanceof Integer) {
+                argumentContent.add(ByteBuffer.allocate(Byte.BYTES + Integer.BYTES)
+                        .put(UPCOMING_INT)
+                        .putInt((int) obj)
+                        .array());
+                argumentContentLength += Byte.BYTES + Integer.BYTES;
+            } else if (obj instanceof Float) {
+                argumentContent.add(ByteBuffer.allocate(Byte.BYTES + Float.BYTES)
+                        .put(UPCOMING_FLOAT)
+                        .putFloat((float) obj)
+                        .array());
+                argumentContentLength += Byte.BYTES + Float.BYTES;
+            }
+        }
 
-        // retrieve the message string
-        String message = new String(Arrays.copyOfRange(packetData, index, index + messageLen), StandardCharsets.UTF_16);
+        ByteBuffer marshalledClientRequest = ByteBuffer.allocate(2 * Integer.BYTES + argumentContentLength);
+        marshalledClientRequest
+                .putInt(requestId)
+                .putInt(opType)
+                .putInt(argumentContent.size());
 
-        return new Response(statusValues[statusType], message);
-    }
+        for (byte[] bytes : argumentContent)
+            marshalledClientRequest.put(bytes);
 
-    public static byte[] marshalServerResponse(Response resp) {
-        int statusType = resp.getStatus().ordinal();
-        String messageString = resp.getMessage();
-        int messageLen = messageString.length();
-
-        int contentLen = Integer.BYTES + messageLen;
-        ByteBuffer marshalledMesage = ByteBuffer.allocate(contentLen);
-
-        marshalledMesage
-                .putInt(statusType)
-                .putInt(messageLen)
-                .put(messageString.getBytes());
-
-        return marshalledMesage.array();
+        return marshalledClientRequest.array();
     }
 
     public static Request unmarshalClientRequest(byte[] packetData) {
@@ -88,9 +99,37 @@ public class MessageHandler {
         return new Request(requestId, opType, arguments);
     }
 
-    // TODO: implement marshalling
-    public static byte[] marshalClientRequest(Request resp) {
-        return new byte[1024];
+    public static byte[] marshalServerResponse(Response resp) {
+        int statusType = resp.getStatus().ordinal();
+        String messageString = resp.getMessage();
+        int messageLen = messageString.length();
+
+        int contentLen = Integer.BYTES + messageLen;
+        ByteBuffer marshalledServerResponse = ByteBuffer.allocate(contentLen);
+
+        marshalledServerResponse
+                .putInt(statusType)
+                .putInt(messageLen)
+                .put(messageString.getBytes());
+
+        return marshalledServerResponse.array();
     }
 
+    public static Response unmarshalServerResponse(byte[] packetData) {
+        int index = 0;
+        ByteBuffer extractor = ByteBuffer.wrap(packetData);
+
+        // retrive type of status (FAILURE / SUCCESS)
+        int statusType = extractor.getInt(index);
+        index += Integer.BYTES;
+
+        // retrieve length of the message string
+        int messageLen = extractor.getInt(index);
+        index += Integer.BYTES;
+
+        // retrieve the message string
+        String message = new String(Arrays.copyOfRange(packetData, index, index + messageLen), StandardCharsets.UTF_16);
+
+        return new Response(statusValues[statusType], message);
+    }
 }
